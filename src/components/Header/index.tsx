67e0efd2d167fac844f7ff9067f1fe4702ba01ae
ramon <ramon.ubt@gmail.com>
2024-02-26 16:52:55 -0300

    feats: adicionando paleta de cores e dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import ToggleTheme from "../ToggleTheme";
 
 const Header = () => {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">
         Hello User
       </span>
--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -1,13 +1,24 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/16/solid";
+import { useEffect } from "react";
 
 const ToggleTheme = () => {
+  const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const pageClasses = document.documentElement.classList;
+
   const toggleTheme = () => {
-    document.documentElement.classList.toggle("dark");
+    pageClasses.toggle("dark");
   };
+
+  useEffect(() => {
+    if (systemPreference) {
+      pageClasses.add("dark");
+    }
+  });
+  
   return (
     <div>
-      <MoonIcon onClick={ toggleTheme } className="h-8 text-gray-100 cursor-pointer hidden dark:block" />
-      <SunIcon onClick={ toggleTheme } className="h-8 text-gray-100 cursor-pointer block dark:hidden" />
+      <MoonIcon onClick={ toggleTheme } className="h-8 text-gray-100 cursor-pointer block dark:hidden" />
+      <SunIcon onClick={ toggleTheme } className="h-8 text-gray-100 cursor-pointer hidden dark:block" />
     </div>
   );
 };
